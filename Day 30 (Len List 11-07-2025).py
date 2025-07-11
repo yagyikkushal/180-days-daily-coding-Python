cfcd127b7ec4dbe1f180bbe24f7f6901e8d551ed
Kushal Yagyik <57190976+yagyikkushal@users.noreply.github.com>
2025-07-11 22:53:25 +0530

    Day 30 (Len List 11-07-2025)

---
+++ Day 30 (Len List 11-07-2025).py
@@ -0,0 +1,10 @@
+# Write a Python program to find the length of a given list of non-empty strings.
+
+mylist = ['cat', 'dog', 'shatter', 'donut', 'at', 'todo', '']
+
+lenlist = []
+
+
+lenlist = [len(_) for _ in mylist]
+
+print(lenlist)
