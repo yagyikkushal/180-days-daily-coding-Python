507c872c3fe9d1100cdc80edc93419cf6d926de0
Kushal Yagyik <57190976+yagyikkushal@users.noreply.github.com>
2025-07-14 23:54:56 +0530

    Day 33 (List and Tuple 14-07-2025)

---
+++ Day 33 (List and Tuple 14-07-2025).py
@@ -0,0 +1,13 @@
+mylist = [([1, 3, 2, 32, 19], [19, 2, 48, 19], [], [9, 35, 4], [3, 19]), 19]
+mylist2 = []
+
+key = mylist[1]
+
+for i in mylist[0]:
+    for j, k in enumerate(i):
+        if k == key:
+            mylist2.append([mylist[0].index(i), i.index(k)])
+        else:
+            continue
+
+print(mylist2)
