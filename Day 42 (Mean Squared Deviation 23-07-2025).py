6824401ce6aa2a666234f7c056f3aedd76b29b13
Kushal Yagyik <57190976+yagyikkushal@users.noreply.github.com>
2025-07-23 21:32:23 +0530

    Day 42 (Mean Squared Deviation 23-07-2025)

---
+++ Day 42 (Mean Squared Deviation 23-07-2025).py
@@ -0,0 +1,7 @@
+# Write a Python program to find x that minimizes the mean squared deviation from a given list of numbers.
+
+mylist = [12, -2, 14, 3, -15, 10, -45, 3, 30]
+
+print(sum(mylist)/len(mylist))
+
+# So mean is that number (x) which minimizes the mean squared deviation.
